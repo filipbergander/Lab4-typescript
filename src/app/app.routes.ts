cac4d9619658cdcab09ea2909b763b04a8e7418d
Filip Bergander <filip.bergander1996@gmail.com>
2026-04-28 10:52:04 +0200

    Skapat struktur på Table och lagt in course-komponenten inom sidan Home. Pipe för uppercase hämtas in och används inom HTML-koden för table.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { NotFound } from './pages/not-found/not-found';
-import { CourseComponent } from './pages/course/course';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
-    { path: 'course', component: CourseComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '404', component: NotFound },
     { path: '**', redirectTo: '404', pathMatch: 'full' }
--- src/app/pages/course/course.ts
+++ src/app/pages/course/course.ts
@@ -2,11 +2,11 @@ import { Component, effect, inject, signal } from '@angular/core';
 import { CourseService } from '../../services/get-course';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Course } from '../../interface/course';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course',
-
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course.html',
   styleUrl: './course.scss',
 })
